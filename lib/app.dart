34bfa7051611117c1dfe35d27f7c031dc4401878
Yash-Garg <yash.garg@outlook.in>
2021-05-18 11:48:58 +0530

    Add basic files for register and login
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,22 @@
+import 'package:easybuy/screens/login.dart';
+import 'package:easybuy/screens/register.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-List<GetPage> _pages = [];
+List<GetPage> _pages = [
+  GetPage(
+    name: '/register',
+    page: () => RegistrationScreen(),
+    transition: Transition.topLevel,
+    transitionDuration: Duration(milliseconds: 200),
+  ),
+  GetPage(
+    name: '/login',
+    page: () => LoginScreen(),
+    transition: Transition.topLevel,
+    transitionDuration: Duration(milliseconds: 200),
+  ),
+];
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,7 +24,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'EasyBuy',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/register',
       getPages: _pages,
     );
   }
